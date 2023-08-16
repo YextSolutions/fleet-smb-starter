287b9fde11bc9d1f054ac5d38f99214f3a14f7ea
lymarrie <lucmarrie1@gmail.com>
2023-08-16 15:27:36 -0400

    client template

---
+++ src/templates/_client.tsx
@@ -0,0 +1,26 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { PageContext } from "@yext/pages";
+
+export { render };
+
+const render = async (pageContext: PageContext<any>) => {
+  const { Page, pageProps } = pageContext;
+
+  let backgroundColor;
+
+  if (pageProps.document.c_backgroundColor) {
+    const transformedColor = pageProps.document.c_backgroundColor.replace(/\s+/g, '').toLowerCase();
+    backgroundColor = `--backgroundColor: ${transformedColor}`;
+  } else {
+    backgroundColor = `--backgroundColor: white`;
+  }
+
+  console.log(pageProps);
+  ReactDOM.hydrate(
+    <Page {...pageProps}>
+        <style>:root {`{${backgroundColor}}`}</style>    
+    </Page>,
+    document.getElementById("reactele")
+  );
+};
